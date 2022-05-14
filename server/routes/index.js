f0b61b968a19c27d49ea3941993f5d054351b9ac
roblesvargas97 <roblesvargas97@gmail.com>
2022-05-14 03:06:51 -0500

    eslint to routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,10 @@ const usersRoutes = require("./usersRoutes");
 const usersBeach = require("./usersBeach");
 
 function routerApi(app) {
-  const router = express.Router();
-  app.use("/api/v1", router);
-  router.use("/users", usersRoutes);
-  router.use("/usersBeach", usersBeach);
+    const router = express.Router();
+    app.use("/api/v1", router);
+    router.use("/users", usersRoutes);
+    router.use("/usersBeach", usersBeach);
 }
 
 module.exports = routerApi;
