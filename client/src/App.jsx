3a60291b1f3944aa0a790e0805b1083fe07cb5ae
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-05-14 14:07:19 -0500

    Add: components, pages, styles and folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './pages/Home'
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hackathon</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
+      <h1>Action for the ocean</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Map from "../components/Map";
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <Map />
+    </div>
+  );
+};
+
+export default Home;
