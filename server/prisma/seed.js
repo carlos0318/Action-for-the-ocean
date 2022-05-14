5087a29519e9c596381db258e2a0e425548c0d55
hpoggi <hectorbcpoggi@gmail.com>
2022-05-14 10:58:15 -0500

    Add: models and seed file

---
+++ server/prisma/seed.js
@@ -0,0 +1,82 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+(async function main() {
+    try {
+        const user1 = await prisma.user.upsert({
+            where: { email: "juan@dominio.com" },
+            update: {},
+            create: {
+                name: "Juan",
+                lastname: "Perez Lopez",
+                email: "juan@dominio.com",
+                password: "juan123",
+                country: "Mexico",
+                birthday: new Date("3/10/1996")
+				
+            },
+        });
+
+        const user2 = await prisma.user.upsert({
+            where: { email: "pedro@dominio.com" },
+            update: {},
+            create: {
+                name: "Pedro",
+                lastname: "Hernandez Gutierrez",
+                email: "pedro@dominio.com",
+                password: "pedro123",
+                country: "Italia",
+                birthday: new Date("4/26/1992")
+				
+            },
+        });
+
+        const user3 = await prisma.user.upsert({
+            where: { email: "ana.parra@dominio.com" },
+            update: {},
+            create: {
+                name: "Ana",
+                lastname: "Parra Novo",
+                email: "ana.parra@dominio.com",
+                password: "ana.parra123",
+                country: "Japon",
+                birthday: new Date("5/5/1995")
+				
+            },
+        });
+
+        const user4 = await prisma.user.upsert({
+            where: { email: "abril.mendez@dominio.com" },
+            update: {},
+            create: {
+                name: "Abril",
+                lastname: "Legorreta Mendez",
+                email: "abril.mendez@dominio.com",
+                password: "abril.mendez123",
+                country: "España",
+                birthday: new Date("7/7/1998")
+				
+            },
+        });
+	
+        const user5 = await prisma.user.upsert({
+            where: { email: "brian.sanchez@dominio.com" },
+            update: {},
+            create: {
+                name: "Brian",
+                lastname: "Sanchez Cabrera",
+                email: "brian.sanchez@dominio.com",
+                password: "brian.sanchez123",
+                country: "Chile",
+                birthday: new Date("8/28/2001")				
+            },
+        });
+
+
+    } catch(e) {
+        console.error(e);
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect();
+    }
+})();
