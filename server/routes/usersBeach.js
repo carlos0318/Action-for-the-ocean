d13a5d4ae2d44f90f3614ff354c6beea82e7b8b6
roblesvargas97 <roblesvargas97@gmail.com>
2022-05-14 02:01:06 -0500

    Create usersBeach

---
+++ server/routes/usersBeach.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/locations", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+router.post("/rating", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+module.exports = router;
