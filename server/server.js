535b5947204ac7001413a8cf641a9eaf82eee3dd
roblesvargas97 <roblesvargas97@gmail.com>
2022-05-14 03:07:26 -0500

    Modify server

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const routerApi = require("./routes");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", (res) => {
+    res.send("Hello World");
 });
 
 routerApi(app);
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+    console.log(`app listening on port ${port}`);
 });
