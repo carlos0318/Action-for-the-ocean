c1fd1b692ec7239b6135a98d3c34c9e2436c1c90
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-05-14 14:39:37 -0500

    Add changes links to home, login and register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import '../styles/Register.css'
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='container'>
+      <img src="../../public/img/wave.svg" alt="wave" className="img" />
+        <div className="register-container">
+            <h1>Registro</h1>
+            <form action="">
+                <input type="text" name="nombre" id="" placeholder="Nombre" />
+                <input type="text" name="apellido" id="" placeholder="Apellido" />
+                <input type="email" name="email" id="" placeholder="Email" />
+                <input type="text" name="nacionalidad" id="" placeholder="Nacionalidad" />
+                <input type="password" name="contraseña" id="" placeholder="Contraseña" />
+                <input type="password" name="contraseña2" id="" placeholder="Confirmar Contraseña" />
+        
+                <input type="submit" value="Registrar" />
+            </form>
+            <div className="register-footer">
+                <p>¿Ya tienes cuenta?</p>
+                <Link to="/login">Iniciar Sesion</Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
