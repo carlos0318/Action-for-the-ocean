3864b08fb545795d88974fd47d1079571491a2ec
roblesvargas97 <roblesvargas97@gmail.com>
2022-05-14 03:06:13 -0500

    connect usersBeach and usersRouters with controllers

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,20 +1,42 @@
 const express = require("express");
+const UserController = require("../controllers/UserController");
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  try {
-  } catch (error) {}
+    try {
+        const body = req.body;
+        const login = await UserController.login(body);
+        return login;
+    } catch (error) {
+        res.status(404).json({
+            message: error.message,
+        });
+    }
 });
 
 router.post("/register", async (req, res) => {
-  try {
-  } catch (error) {}
+    try {
+        const body = req.body;
+        const register = await UserController.register(body);
+        return register;
+    } catch (error) {
+        res.status(404).json({
+            message: error.message,
+        });
+    }
 });
 
 router.put("/update", async (req, res) => {
-  try {
-  } catch (error) {}
+    try {
+        const body = req.body;
+        const update = await UserController.updateUser(body);
+        return update;
+    } catch (error) {
+        res.status(404).json({
+            message: error.message,
+        });
+    }
 });
 
 module.exports = router;
