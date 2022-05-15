509c5542a056b937f16c7b753f8f9ead2e47ff63
roblesvargas97 <roblesvargas97@gmail.com>
2022-05-14 20:04:28 -0500

    Create UserService

---
+++ server/services/UserService.js
@@ -0,0 +1,66 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+class UserService {
+
+    static async register (user) {
+        const message = "Usuario Creado";
+        await prisma.user.create({data: user});
+        return message;
+    }
+
+    static async login (user) {
+        const query = await prisma.user.findFirst({
+            where: {
+                email: user.email,
+                password: user.password
+            }
+        });
+        if(query === null){
+            throw new Error("Usuario o Contraseña incorrecto");
+        }else{
+            return query.email;
+        }
+    }
+
+    static async getUserData(user){
+        const query = await prisma.user.findFirst({
+            where: {
+                email: user.email,
+            }
+        });
+        if(query === null){
+            throw new Error("Usuario o Contraseña incorrecto");
+        }else{
+            const { name , lastname , email , password , country } = query ;
+
+            return{
+                name,
+                lastname,
+                email,
+                password,
+                country
+            };
+        }
+    }
+
+    static async updateUser(user){
+        const query = await prisma.user.update({
+            where:{
+                email: user.email
+            },
+            data:{
+                name: user.name,
+                lastname: user.lastname,
+                email: user.email,
+                password: user.password,
+                country: user.country
+            }
+        });
+
+        return query;
+    }
+
+}
+
+module.exports = UserService;
