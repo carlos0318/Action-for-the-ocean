85398589f6ada3635a3b95f56833217390a8498e
roblesvargas97 <roblesvargas97@gmail.com>
2022-05-14 21:26:08 -0500

    Modify usersBeach

--- server/routes/usersBeach.js
+++ server/routes/usersBeach.js
@@ -5,9 +5,17 @@ const router = express.Router();
 
 router.post("/locations", async (req, res) => {
     try {
-        const body = req.body;
-        const createLocation = UserBeachController.createLocation(body);
-        return createLocation;
+        const userData = {
+            userId: req.body.userId,
+            beachId: req.body.beachId,
+            latitude: req.body.latitude,
+            longitude: req.body.longitude,
+            rating: req.body.rating
+        };
+        const createLocation = UserBeachController.createLocation(userData);
+        return res.status(200).json({
+            message: createLocation
+        });
     } catch (error) {
         res.status(404).json({
             message: error.message,
