f6126a4c398293d48f64ba83f23408b99a117064
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-05-15 19:22:28 -0500

    fixed margin stars

--- client/src/pages/UserPoint.jsx
+++ client/src/pages/UserPoint.jsx
@@ -10,7 +10,7 @@ import axios from "axios";
 const UserPoint = () => {
   const { user } = useContext(UserContext);
   const [selectedStars, setSelectedStars] = useState(0);
-  const [rating, setRating] = useState(selectedStars * 2);
+  const [rating, setRating] = useState(0);
   const coordenadas = [21.146743185077675, -86.78885941594436];
   const [coordinates, setCoordinates] = useState([]);
   const navigate = useNavigate();
@@ -32,11 +32,12 @@ const UserPoint = () => {
   const handleCreateNewPoint = async (e) => {
     e.preventDefault();
     try {
+      console.log(selectedStars *2)
       await axios.post("http://localhost:4000/api/v1/usersBeach/", {
         userId: user.id,
         latitude: coordinates.lat,
         longitude: coordinates.lng,
-        rating: parseInt(rating),
+        rating: parseInt(selectedStars)*2,
       });
       console.log("created");
     } catch (error) {
@@ -52,7 +53,7 @@ const UserPoint = () => {
         Aquí podrás crear una alerta de que en la playa hay mucha basura. Y
         decirnos de la escala del 0 al 10 de que tan sucia esta la playa.
       </p>
-      <div>
+      <div className="stars-container">
         <label htmlFor="rating">Rating:</label>
         {/* <input
           type="number"
@@ -63,13 +64,15 @@ const UserPoint = () => {
           value={rating}
           onChange={(e) => setRating(e.target.value)}
         /> */}
-        {[...Array(5)].map((n, i) => (
-          <Star
-            key={i}
-            selected={selectedStars > i}
-            onSelect={() => setSelectedStars(i + 1)}
-          />
-        ))}
+        <div className="stars2">
+          {[...Array(5)].map((n, i) => (
+            <Star
+              key={i}
+              selected={selectedStars > i}
+              onSelect={() => setSelectedStars(i + 1)}
+            />
+          ))}
+        </div>
       </div>
       <button className="userpoint-button" onClick={handleCreateNewPoint}>
         Crear una nueva alerta
